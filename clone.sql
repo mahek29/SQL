2e83964b9b8377411080abc92cf34e39fafe0d41
Mahek <chadhataranpreet02@gmail.com>
2021-04-23 01:59:40 +0530

    schema

---
+++ clone.sql
@@ -0,0 +1,63 @@
+CREATE DATABASE clone;
+USE clone;
+
+CREATE TABLE users(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) UNIQUE NOT NULL,
+    created_at DATETIME DEFAULT NOW()
+);
+
+CREATE TABLE photos(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    image_url VARCHAR(100) NOT NULL,
+    user_id INT NOT NULL,
+    created_at DATETIME DEFAULT NOW();
+    FOREIGN KEY(user_id) REFERENCES users(id)
+);
+
+CREATE TABLE comments(
+     id INT AUTO_INCREMENT PRIMARY KEY,
+     comment VARCHAR(500) NOT NULL,
+     user_id INT NOT NULL,
+     photo_id INT NOT NULL,
+     created_at DATETIME DEFAULT NOW(),
+     FOREIGN KEY(user_id) REFERENCES users(id),
+     FOREIGN KEY(photo_id) REFERENCES photos(id)
+);
+
+CREATE TABLE likes(
+    user_id INT NOT NULL,
+    photo_id INT NOT NULL,
+    created_at DATETIME DEFAULT NOW(),
+    FOREIGN KEY(user_id) REFERENCES users(id),
+    FOREIGN KEY(photo_id) REFERENCES photos(id),
+    PRIMARY KEY(user_id,photo_id)
+);
+
+CREATE TABLE follows(
+    follower_id INT NOT NULL,
+    followee_id INT NOT NULL,
+    created_at DATETIME DEFAULT NOW(),
+    FOREIGN KEY(follower_id) REFERENCES users(id),
+    FOREIGN KEY(followee_id) REFERENCES users(id),
+    PRIMARY KEY(follower_id,followee_id)
+);
+
+CREATE TABLE tags(
+     id INT AUTO_INCREMENT PRIMARY KEY,
+     tagname VARCHAR(255) UNIQUE,
+     created_at DATETIME DEFAULT NOW(),
+);
+
+CREATE TABLE phototags(
+    photo_id INT NOT NULL,
+    tag_id INT NOT NULL,
+    FOREIGN KEY(photo_id) REFERENCES photos(id),
+    FOREIGN KEY(tag_id) REFERENCES tags(id),
+    PRIMARY KEY(photo_id,tag_id)
+);
+
+SELECT photos.image_url,users.username
+FROM users
+JOIN photos
+   ON users.id=photos.user_id;
